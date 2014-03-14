771e9f7240e12671edc80dd48f2c87d0cf202e35
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-03-14 10:29:39 -0400

    Switch to chalk.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-'use strict';
-
 var path = require('path');
 var childProcess = require('child_process');
 var gutil = require('gulp-util');
+var chalk = require('chalk');
 var through = require('through2');
 var phantomjs = require('phantomjs');
 var binPath = phantomjs.path;
 
 module.exports = function(){
+    'use strict';
     return through.obj(function (file, enc, cb) {
         var childArgs = [
             path.join(__dirname, 'runner.js'),
             file.path
         ];
 
-        if (file.isNull()) {
-            this.push(file);
+        if (file.isStream()) {
+            this.emit('error', new gutil.PluginError('gulp-qunit', 'Streaming not supported'));
             return cb();
         }
 
@@ -23,12 +23,12 @@ module.exports = function(){
             console.log(stdout);
 
             if (stderr !== '') {
-                gutil.log('gulp-qunit: Failed to open test runner ' + gutil.colors.blue(file.relative));
+                gutil.log('gulp-qunit: Failed to open test runner ' + chalk.blue(file.relative));
                 this.emit('error', new gutil.PluginError('gulp-qunit', stderr));
             }
 
             if (err !== null) {
-                gutil.log('gulp-qunit: ' + gutil.colors.red("✖ ") + 'QUnit assertions failed in ' + gutil.colors.blue(file.relative));
+                gutil.log('gulp-qunit: ' + chalk.red('✖ ') + 'QUnit assertions failed in ' + chalk.blue(file.relative));
                 this.emit('error', new gutil.PluginError('gulp-qunit', err));
             }
 
