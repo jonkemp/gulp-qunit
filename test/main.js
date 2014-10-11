88666c4751d520c402593dc79ea7eeb07bceb3cc
floriansemm <floriansemm@gmail.com>
2014-10-11 11:33:00 +0200

    add testcase that checks finished event emitting

--- test/main.js
+++ test/main.js
@@ -8,6 +8,8 @@ var path = require('path');
 var qunit = require('../index');
 var out = process.stdout.write.bind(process.stdout);
 
+var events = require('events')
+
 describe('gulp-qunit', function() {
     it('tests should pass', function(cb) {
         this.timeout(5000);
@@ -53,5 +55,35 @@ describe('gulp-qunit', function() {
         }));
 
         stream.end();
+    });
+
+    it('tests should pass and emit finished event', function(cb) {
+        this.timeout(5000);
+
+        var stream = qunit();
+
+        stream.on('gulp-qunit.finished', function(passed) {
+            assert(true, 'phantom finished with errors');
+        })
+
+        process.stdout.write = function (str) {
+            //out(str);
+
+            if (/10 passed. 0 failed./.test(str)) {
+                assert(true);
+                process.stdout.write = out;
+                cb();
+            }
+        };
+
+        stream.write(new gutil.File({
+            path: path.resolve('./qunit/test-runner.html'),
+            contents: new Buffer('')
+        }));
+
+        stream.end();
+
+
+
     });
 });
