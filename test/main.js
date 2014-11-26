b10eee31e72f9a664af83fb0986edbdd7226352c
Johan Walles <johan.walles@gmail.com>
2014-11-26 22:20:44 +0100

    Actually pass phantomjs-options on to PhantomJS
    Before this change, options specified using...
    
    qunit({'phantomjs-options': ['--ssl-protocol=any']})
    
    ... were silently dropped by gulp-qunit.
    
    The test case for this change passes --help to PhantomJS and verifies
    that:
    a) No tests are run
    b) Some help is received
    
    Without the changes to index.js, this test case fails.

--- test/main.js
+++ test/main.js
@@ -56,6 +56,40 @@ describe('gulp-qunit', function() {
         stream.end();
     });
 
+    it('tests should not run when passing --help to PhantomJS', function(cb) {
+        this.timeout(5000);
+
+        var stream = qunit({'phantomjs-options': ['--help']});
+
+        process.stdout.write = function (str) {
+            //out(str);
+
+            if (/10 passed. 0 failed./.test(str)) {
+                assert(false, 'No tests should run when passing --help to PhantomJS');
+                process.stdout.write = out;
+                cb();
+                return;
+            }
+
+            var lines = str.split('\n');
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i];
+                if (/.*--help.*Shows this message and quits/.test(line)) {
+                    assert(true);
+                    process.stdout.write = out;
+                    cb();
+                }
+            }
+        };
+
+        stream.write(new gutil.File({
+            path: './qunit/test-runner.html',
+            contents: new Buffer('')
+        }));
+
+        stream.end();
+    });
+
     it('tests should pass with absolute source paths', function(cb) {
         this.timeout(5000);
 
