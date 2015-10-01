8cdc621209e519e1f97b938849fdca352edd7f92
Jonathan Kemp <jkemp@scrippsnetworks.com>
2015-10-01 16:15:48 -0400

    Add support for JSCS.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     mocha = require('gulp-mocha'),
-    qunit = require('./index');
+    qunit = require('./index'),
+    jscs = require('gulp-jscs');
 
 var paths = {
     scripts: ['./*.js', './test/*.js', '!./gulpfile.js']
@@ -20,13 +21,18 @@ gulp.task('test', function() {
         .pipe(mocha({reporter: 'dot'}));
 });
 
+gulp.task('jscs', function () {
+    return gulp.src(paths.scripts)
+        .pipe(jscs());
+});
+
 gulp.task('qunit', function() {
     return gulp.src('./test/fixtures/passing.html')
         .pipe(qunit());
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.scripts, ['lint', 'test']);
+    gulp.watch(paths.scripts, ['lint', 'jscs', 'test']);
 });
 
-gulp.task('default', ['lint', 'test', 'watch']);
+gulp.task('default', ['lint', 'jscs', 'test', 'watch']);
